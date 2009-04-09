32455e0d3499716fa184651ab472c34bfa8a3e64
Tanel Tammet <tanel.tammet@gmail.com>
2009-04-09 06:19:57 +0000

    tanel: compile.sh fix

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@
 
 # alternative to compilation with automake/make: just run 
 
-gcc  -O2 -Wall -o Main/wgdb Main/wgdb.c Db/dbmem.c Db/dballoc.c
+gcc  -O2 -Wall -o Main/wgdb Main/wgdb.c Db/dbmem.c Db/dballoc.c Db/dbdata.c Db/dbindex.c Db/dbtest.c
 
