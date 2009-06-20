d62321113c40e235cd85daf8ed0a133848bc9e2b
Priit Järv <priit@cc.ttu.ee>
2009-06-20 14:14:32 +0000

    added static configuration for gcc manual build

--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@
 
 # alternative to compilation with automake/make: just run 
 
-[ -f config.h ] || touch config.h
-gcc  -O2 -Wall -o Main/wgdb Main/wgdb.c Db/dbmem.c Db/dballoc.c Db/dbdata.c Db/dbindex.c Db/dbtest.c
+[ -f config.h ] || cp config-gcc.h config.h
+gcc  -O2 -Wall -o Main/wgdb Main/wgdb.c Db/dbmem.c Db/dballoc.c Db/dbdata.c Db/dbindex.c Db/dbtest.c Db/dbdump.c
 gcc  -O2 -o Main/indextool  Main/indextool.c Db/dbmem.c Db/dballoc.c Db/dbdata.c Db/dbindex.c Db/dbtest.c
---
+++ config-gcc.h
@@ -0,0 +1,42 @@
+/*
+ * XXX: put this file under license if needed
+ *
+ */
+
+ /** @file config-gcc.h
+ * Build configuration for gcc platform.
+ *
+ * Based on auto-generated config.h. Should be manually synced whenever
+ * additional configuration parameters are added.
+ */
+
+/* Define if you have POSIX threads libraries and header files. */
+#define HAVE_PTHREAD 1
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "wgandalf"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Define to necessary symbol if this constant uses a non-standard name on
+   your system. */
+/* #undef PTHREAD_CREATE_JOINABLE */
+
+/* Version number of package */
+#define VERSION "0.2.0"
