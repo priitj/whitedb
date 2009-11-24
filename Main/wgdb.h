6cdc288f36b749f89dd7bc6f808feec985837890
Priit Järv <priit@cc.ttu.ee>
2009-11-24 14:48:58 +0000

    separated DB usage examples from wgdb commandline tool. Added Examples directory to the autotools build

--- Main/wgdb.h
+++ Main/wgdb.h
@@ -30,14 +30,6 @@
 #ifndef __defined_wgdb_h
 #define __defined_wgdb_h
 
-int db_write(void* db);
-int db_read(void* db);
-int db_test5(void* db);
-int db_test4(void* db);
-int db_test3a(void* db);
-int db_test3(void* db);
-int db_test2(void* shmptr);
-int db_test1(void* shmptr);
 
 
 #endif
