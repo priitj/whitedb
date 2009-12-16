df6380a7b73127693dc7b23963e88fc18d78832b
Priit Järv <priit@cc.ttu.ee>
2009-12-16 08:15:25 +0000

    compile.sh was broken due to db init always requiring wg_init_locks(). Fixed by adding the source file

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@
 # alternative to compilation with automake/make: just run 
 
 [ -f config.h ] || cp config-gcc.h config.h
-gcc  -O2 -Wall -o Main/wgdb Main/wgdb.c Db/dbmem.c Db/dballoc.c Db/dbdata.c Db/dbindex.c Db/dbtest.c Db/dbdump.c  Db/dblog.c Db/dbhash.c
-gcc  -O2 -o Main/indextool  Main/indextool.c Db/dbmem.c Db/dballoc.c Db/dbdata.c Db/dbindex.c Db/dbtest.c  Db/dblog.c Db/dbhash.c
+gcc  -O2 -Wall -march=pentium4 -o Main/wgdb Main/wgdb.c Db/dbmem.c Db/dballoc.c Db/dbdata.c Db/dblock.c Db/dbindex.c Db/dbtest.c Db/dbdump.c  Db/dblog.c Db/dbhash.c
+gcc  -O2 -march=pentium4 -o Main/indextool  Main/indextool.c Db/dbmem.c Db/dballoc.c Db/dbdata.c Db/dblock.c Db/dbindex.c Db/dbtest.c  Db/dblog.c Db/dbhash.c
 gcc  -O3 -Wall -march=pentium4 -pthread -o Main/stresstest Main/stresstest.c Db/dbmem.c Db/dballoc.c Db/dbdata.c Db/dblock.c Db/dbtest.c  Db/dblog.c Db/dbhash.c
