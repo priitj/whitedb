a5dea464e0f7b009dc44f8f68a37d5dafaa77572
Priit Järv <priit@cc.ttu.ee>
2009-12-21 10:31:03 +0000

    added DLL building for Win32 platform

--- compile.bat
+++ compile.bat
@@ -1,3 +1,11 @@
 cl /Ox /W3 Main\wgdb.c Db\dbmem.c Db\dballoc.c Db\dbdata.c Db\dblock.c Db\dbtest.c DB\dbdump.c Db\dblog.c Db\dbhash.c
 cl /Ox /W3 Main\stresstest.c Db\dbmem.c Db\dballoc.c Db\dbdata.c Db\dblock.c Db\dbtest.c Db\dblog.c Db\dbhash.c
 
+@rem build DLL. Currently we are not using it to link executables however.
+@rem unlike gcc build, it is necessary to have all functions declared in
+@rem wgdb.def file. Make sure it's up to date (should list same functions as
+@rem Db/dbapi.h)
+cl /Ox /W3 /MT /Fewgdb /LD Db\dbmem.c Db\dballoc.c Db\dbdata.c Db\dblock.c Db\dbtest.c DB\dbdump.c Db\dblog.c Db\dbhash.c /link /def:wgdb.def /incremental:no /MANIFEST:NO
+
+@rem Example of linking against wgdb.dll
+@rem cl /Ox /W3 Main\stresstest.c wgdb.lib
