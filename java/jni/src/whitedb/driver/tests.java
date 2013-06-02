44efb88a59cb35582befeaa9bf06cf1bce1a4d15
Priit Järv <priit@cc.ttu.ee>
2013-06-02 12:21:40 +0300

    added license headers to Java API code

--- java/jni/src/whitedb/driver/tests.java
+++ java/jni/src/whitedb/driver/tests.java
@@ -1,3 +1,32 @@
+/*
+* $Id:  $
+* $Version: $
+*
+* Copyright (c) Andres Puusepp 2009
+* Copyright (c) Priit Järv 2013
+*
+* This file is part of WhiteDB
+*
+* WhiteDB is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* WhiteDB is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with WhiteDB.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+ /** @file tests.java
+ *  Java API tests and demos.
+ *
+ */
+
 package whitedb.driver;
 
 import whitedb.driver.WhiteDB;
