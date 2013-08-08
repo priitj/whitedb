30bca092d9dda61d8294b331dda02d8e464f8765
Priit Järv <priit@cc.ttu.ee>
2013-08-08 13:38:12 +0300

    updated compiling Java bindings under Windows

--- java/jni/compileBridge.bat
+++ java/jni/compileBridge.bat
@@ -1,3 +1,3 @@
 cd library
-"C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\cl.exe" /LD /I"C:\Program Files\Java\jdk1.6.0_14\include" /I"C:\Program Files\Java\jdk1.6.0_14\include\win32" /W3 ..\src\native\whitedbDriver.c ..\..\..\Db\dbmem.c ..\..\..\Db\dballoc.c ..\..\..\Db\dbdata.c ..\..\..\Db\dbindex.c ..\..\..\Db\dbtest.c
+"C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\cl.exe" /LD /I"C:\Program Files\Java\jdk1.7.0_25\include" /I"C:\Program Files\Java\jdk1.7.0_25\include\win32" /W3 ..\src\native\whitedbDriver.c ..\..\..\Db\dbmem.c ..\..\..\Db\dballoc.c ..\..\..\Db\dbdata.c ..\..\..\Db\dbindex.c ..\..\..\Db\dbtest.c ..\..\..\Db\dbquery.c ..\..\..\Db\dbutil.c ..\..\..\Db\dbmpool.c ..\..\..\Db\dblock.c ..\..\..\Db\dbcompare.c ..\..\..\Db\dbhash.c
 cd ..
--- java/jni/setEnv.bat
+++ java/jni/setEnv.bat
@@ -1,4 +1,4 @@
-PATH=%PATH%;C:\Program Files\Java\jdk1.6.0_14\bin\;
+PATH=%PATH%;C:\Program Files\Java\jdk1.7.0_25\bin\;
 REM cd C:\path\to\whitedb\java\jni\
 PATH=%path%;./library
 "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
