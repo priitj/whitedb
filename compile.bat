83841d403b9b2630da5c17a5d8e664143269acbc
Priit Järv <priit@cc.ttu.ee>
2013-08-28 12:51:15 +0300

    updated manual compilation scripts

--- compile.bat
+++ compile.bat
@@ -1,8 +1,8 @@
 @rem current version does not build reasoner: added later
 
 cl /Ox /W3 Main\wgdb.c Db\dbmem.c Db\dballoc.c Db\dbdata.c Db\dblock.c Db\dbtest.c DB\dbdump.c Db\dblog.c Db\dbhash.c Db\dbindex.c Db\dbcompare.c Db\dbquery.c Db\dbutil.c Db\dbmpool.c Db\dbjson.c Db\dbschema.c json\JSON_parser.c
-cl /Ox /W3 Main\stresstest.c Db\dbmem.c Db\dballoc.c Db\dbdata.c Db\dblock.c Db\dbtest.c Db\dblog.c Db\dbhash.c Db\dbindex.c Db\dbcompare.c Db\dbquery.c Db\dbutil.c Db\dbmpool.c
-cl /Ox /W3 Main\indextool.c Db\dbmem.c Db\dballoc.c Db\dbdata.c Db\dblock.c Db\dbtest.c Db\dblog.c Db\dbhash.c Db\dbindex.c Db\dbcompare.c Db\dbquery.c Db\dbutil.c Db\dbmpool.c
+cl /Ox /W3 Main\indextool.c Db\dbmem.c Db\dballoc.c Db\dbdata.c Db\dblock.c Db\dbtest.c Db\dblog.c Db\dbhash.c Db\dbindex.c Db\dbcompare.c Db\dbquery.c Db\dbutil.c Db\dbmpool.c Db\dbjson.c Db\dbschema.c json\JSON_parser.c
+cl /Ox /W3 Main\stresstest.c Db\dbmem.c Db\dballoc.c Db\dbdata.c Db\dblock.c Db\dblog.c Db\dbhash.c Db\dbindex.c Db\dbcompare.c Db\dbmpool.c
 
 @rem build DLL. Currently we are not using it to link executables however.
 @rem unlike gcc build, it is necessary to have all functions declared in
