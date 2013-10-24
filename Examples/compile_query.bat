93064cf8fa63bf5468e531efcf65203fa56c00d6
Priit Järv <priit@cc.ttu.ee>
2013-10-24 16:30:09 +0300

    added missing source files to Examples compilation scripts

--- Examples/compile_query.bat
+++ Examples/compile_query.bat
@@ -1,1 +1,1 @@
-cl /Ox /W3 /I..\Db query.c ..\Db\dbmem.c ..\Db\dballoc.c ..\Db\dbdata.c ..\Db\dblock.c ..\DB\dbindex.c ..\Db\dblog.c ..\Db\dbhash.c ..\Db\dbcompare.c ..\Db\dbquery.c ..\Db\dbutil.c ..\Db\dbtest.c ..\Db\dbmpool.c
+cl /Ox /W3 /I..\Db query.c ..\Db\dbmem.c ..\Db\dballoc.c ..\Db\dbdata.c ..\Db\dblock.c ..\DB\dbindex.c ..\Db\dblog.c ..\Db\dbhash.c ..\Db\dbcompare.c ..\Db\dbquery.c ..\Db\dbutil.c ..\Db\dbtest.c ..\Db\dbmpool.c ..\Db\dbjson.c ..\Db\dbschema.c ..\json\JSON_parser.c
