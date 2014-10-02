8ed2177cf3403cf3812d9ed2d84578c1be02ac30
necrophcodr <tcg.thegamer@gmail.com>
2014-10-02 11:50:04 +0200

    Fix using $() instead of ${} for COMPILER variable.

--- compile.sh
+++ compile.sh
@@ -28,7 +28,7 @@ fi
 if [ config-gcc.h -nt config.h ]; then
   echo "Warning: config.h is older than config-gcc.h, consider updating it"
 fi
-$(COMPILER) -O2 -Wall -o Main/wgdb Main/wgdb.c Db/dbmem.c \
+${COMPILER} -O2 -Wall -o Main/wgdb Main/wgdb.c Db/dbmem.c \
   Db/dballoc.c Db/dbdata.c Db/dblock.c Db/dbindex.c Db/dbdump.c  \
   Db/dblog.c Db/dbhash.c Db/dbcompare.c Db/dbquery.c Db/dbutil.c Db/dbmpool.c \
   Db/dbjson.c Db/dbschema.c json/yajl_all.c -lm
